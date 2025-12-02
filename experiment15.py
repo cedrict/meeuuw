53ff06ea62b9ec12597f7fd85c5525435de94bec
cedric thieulot <c.thieulot@uu.nl>
2025-12-02 10:15:34 +0100

    exp15updt

---
+++ experiment15.py
@@ -0,0 +1,79 @@
+import numpy as np
+from constants import *
+
+###################################################################################################
+
+nelx=192
+nelz=192
+
+Lx=0.5
+Lz=1
+
+CFLnb=0.
+nstep=1
+eta_ref=1
+every_solution_vtu=1
+every_swarm_vtu=1
+
+nsamplepoints=1
+xsamplepoints=np.array([0.0],dtype=np.float64)
+zsamplepoints=np.array([0.5],dtype=np.float64)
+
+axisymmetric=True
+
+###################################################################################################
+
+def assign_boundary_conditions_V(x_V,z_V,rad_V,theta_V,ndof_V,Nfem_V,nn_V,\
+                                 hull_nodes,top_nodes,bot_nodes,left_nodes,right_nodes):
+
+    bc_fix_V=np.zeros(Nfem_V,dtype=bool) # boundary condition, yes/no
+    bc_val_V=np.zeros(Nfem_V,dtype=np.float64) # boundary condition, value
+
+    for i in range(0,nn_V):
+        if x_V[i]/Lx<eps:
+           bc_fix_V[i*ndof_V  ]=True ; bc_val_V[i*ndof_V  ]=0.
+        if x_V[i]/Lx>(1-eps):
+           bc_fix_V[i*ndof_V  ]=True ; bc_val_V[i*ndof_V  ]=0.
+           bc_fix_V[i*ndof_V+1]=True ; bc_val_V[i*ndof_V+1]=0.
+        if z_V[i]/Lz<eps:
+           bc_fix_V[i*ndof_V+1]=True ; bc_val_V[i*ndof_V+1]=0.
+        if z_V[i]/Lz>(1-eps):
+           bc_fix_V[i*ndof_V+1]=True ; bc_val_V[i*ndof_V+1]=0.
+
+    return bc_fix_V,bc_val_V
+
+###################################################################################################
+
+def particle_layout(nparticle,swarm_x,swarm_z,swarm_rad,swarm_theta,Lx,Lz):
+
+    swarm_mat=np.zeros(nparticle,dtype=np.int32)
+    swarm_mat[:]=1
+
+    for ip in range(0,nparticle):
+        if swarm_x[ip]**2+(swarm_z[ip]-Lz/2)**2<=0.123456789**2:
+           swarm_mat[ip]=2
+
+    return swarm_mat
+
+###################################################################################################
+
+def material_model(nparticle,swarm_mat,swarm_x,swarm_z,swarm_rad,swarm_theta,\
+                   swarm_exx,swarm_ezz,swarm_exz,swarm_T,swarm_p):
+
+    swarm_rho=np.zeros(nparticle,dtype=np.float64)
+    swarm_eta=np.zeros(nparticle,dtype=np.float64)
+    swarm_hcond=0
+    swarm_hcapa=0
+    swarm_hprod=0
+
+    mask=(swarm_mat==1) ; swarm_eta[mask]=1    ; swarm_rho[mask]=1
+    mask=(swarm_mat==2) ; swarm_eta[mask]=1000 ; swarm_rho[mask]=1.01
+
+    return swarm_rho,swarm_eta,swarm_hcond,swarm_hcapa,swarm_hprod
+
+###################################################################################################
+
+def gravity_model(x,z):
+    return 0.,-1.
+
+###################################################################################################
